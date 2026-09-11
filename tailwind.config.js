550f6aab83d583047c5ab38668298d704ace727b
davidhambern-ship-it <davidhambern@gmail.com>
2026-09-10 20:13:41 -0400

    Fix Director modal opacity utility

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
+        opacity: {
+          '97': '0.97',
+        },
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
